242238630e4771891b0f408c3e427e511f2da496
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-11-02 19:39:46 +0200

    Play list Page View

---
+++ lib/views/play_list_screen.dart
@@ -0,0 +1,180 @@
+import 'package:beats/views/widgets/audio_file_widget.dart';
+import 'package:beats/views/widgets/bottom_player_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class PlayListScreen extends StatelessWidget {
+  const PlayListScreen({
+    super.key,
+    required this.playListName,
+  });
+
+  final String playListName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Safi Beats",
+          style: TextStyle(),
+        ),
+        backgroundColor: const Color(0xFF34495E),
+        actions: const [
+          Icon(
+            Icons.search,
+            size: 24.0,
+            color: Color.fromARGB(128, 255, 255, 255),
+          ),
+          SizedBox(
+            width: 23.0,
+          ),
+        ],
+        leading: InkWell(
+          onTap: () {
+            Get.back();
+          },
+          child: const Icon(
+            Icons.arrow_back_ios_new,
+            color: Color.fromARGB(128, 255, 255, 255),
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF34495E),
+                  Color(0xFF161616),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 5.0,
+                        vertical: 15.0,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Text(
+                            playListName,
+                            style: const TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 1',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 2',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 1',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 2',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 1',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 2',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 1',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 2',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 1',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 2',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 1',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 2',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 1',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 2',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 1',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 2',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 1',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 2',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 1',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    AudioFileWidget(
+                      name: 'Sound 2',
+                      time: "${DateTime.now().hour}:${DateTime.now().minute}",
+                    ),
+                    const SizedBox(
+                      height: 80,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const BottomPlayerWidget(),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/widgets/play_list_widget.dart
+++ lib/views/widgets/play_list_widget.dart
@@ -1,4 +1,6 @@
+import 'package:beats/views/play_list_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class PlayListWidget extends StatelessWidget {
   const PlayListWidget({
@@ -12,62 +14,72 @@ class PlayListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 5.0),
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        height: 70,
-        decoration: BoxDecoration(
-          gradient: const LinearGradient(
-            colors: [
-              Color(0xFF2E5A72),
-              Color(0xFF34495E),
-              Color(0xFF2E5A72),
-            ],
-            begin: Alignment.centerRight,
-            end: Alignment.centerLeft,
+    return InkWell(
+      onTap: () {
+        Get.to(
+          () => PlayListScreen(
+            playListName: name,
           ),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Align(
-          alignment: Alignment.center,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Expanded(
-                  child: Row(
-                    children: [
-                      Container(
-                        height: 50,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5)),
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 8.0),
-                          child: Image.asset(
-                            'assets/placeholderImage.jpg',
+          transition: Transition.downToUp,
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5.0),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: 70,
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              colors: [
+                Color(0xFF2E5A72),
+                Color(0xFF34495E),
+                Color(0xFF2E5A72),
+              ],
+              begin: Alignment.centerRight,
+              end: Alignment.centerLeft,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 50,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5)),
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: Image.asset(
+                              'assets/placeholderImage.jpg',
+                            ),
                           ),
                         ),
-                      ),
-                      Text(
-                        name,
-                        style: const TextStyle(
-                          color: Color.fromARGB(128, 255, 255, 255),
+                        Text(
+                          name,
+                          style: const TextStyle(
+                            color: Color.fromARGB(128, 255, 255, 255),
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                Text(
-                  "$numberOfTracks tracks",
-                  style: const TextStyle(
-                    color: Color.fromARGB(128, 255, 255, 255),
+                  Text(
+                    "$numberOfTracks tracks",
+                    style: const TextStyle(
+                      color: Color.fromARGB(128, 255, 255, 255),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
